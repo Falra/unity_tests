b00a50e398fe441fe998f10aaa9adcaa566c99f3
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-01-07 13:37:18 +0200

    Пример с Awake

--- Function Examples/Assets/CodeExample.cs
+++ Function Examples/Assets/CodeExample.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CodeExample : MonoBehaviour
 {
+    public int numTimesCalled = 0;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void Awake() {
+        PrintGameObjectName(this.gameObject);
+        SetColor(Color.red, this.gameObject);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        numTimesCalled++;    
+        //PrintUpdates();
+    }
+
+    void PrintUpdates() {
+        string OutputMassage = "Updates: " + numTimesCalled;
+        print(OutputMassage);
+    }
+
+    void PrintGameObjectName(GameObject obj) {
+        print(obj.name);
+    }
+
+    void SetColor(Color color, GameObject obj) {
+        Renderer r = obj.GetComponent<Renderer>();
+        r.material.color = color;
     }
 }
