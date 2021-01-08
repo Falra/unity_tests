a2da604322fe4d439bac2ae383f64a9da053a9a8
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-01-08 12:41:35 +0200

    Boid p1

--- Boids/Assets/Boid.cs
+++ Boids/Assets/Boid.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set Dynamically")]
+    public Rigidbody rigid;
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+        pos = Random.insideUnitSphere * Spawner.S.spawnRadius;
+        Vector3 vel = Random.onUnitSphere * Spawner.S.velocity;
+        rigid.velocity = vel;
+
+        LookAhead();
+
+        Color randColor = Color.black;
+        while (randColor.r + randColor.g + randColor.b < 1.0f)
+        {
+            randColor = new Color(Random.value, Random.value, Random.value);
+        }
+
+        Renderer[] rends = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in rends)
+        {
+            rend.material.color = randColor;
+        }
+
+        TrailRenderer tRend = GetComponent<TrailRenderer>();
+        tRend.material.SetColor("_TintColor", randColor);
+    }
+    void LookAhead()
+    {
+        transform.LookAt(pos + rigid.velocity);
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 pos
     {
-        
+        get { return transform.position; }
+        set { transform.position = value; }
     }
 }
+
