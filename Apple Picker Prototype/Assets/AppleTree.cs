088a0061710e1b8ed69422f6181c50e5c249bec1
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-03-06 13:14:23 +0200

    AppleTree properties

--- Apple Picker Prototype/Assets/AppleTree.cs
+++ Apple Picker Prototype/Assets/AppleTree.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    // шаблон дерева
+    public GameObject applePrefab;
+    // скорость перемещения
+    public float speed = 1f;
+    // расстояние от края
+    public float leftAndRightEdge = 10f;
+    // вероятность смены направления
+    public float chanceToChangeDirection = 0.1f;
+    // частота падения яблок
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
