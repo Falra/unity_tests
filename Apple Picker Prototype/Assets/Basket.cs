1280ee1a365fb775a2f0b95eaf989a4d2446cb71
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-03-06 17:34:53 +0200

    Basket move

--- Apple Picker Prototype/Assets/Basket.cs
+++ Apple Picker Prototype/Assets/Basket.cs
@@ -13,6 +13,11 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 position = this.transform.position;
+        position.x = mousePos3D.x;
+        this.transform.position = position;    
     }
 }
