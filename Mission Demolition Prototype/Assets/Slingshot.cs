eab7ee0e5abacc8f8ae36b190f85da8cce653019
Valerii Diachenko <leriy.dyachenko@gmail.com>
2021-03-20 13:34:56 +0200

    Projectile creation on mouse down

--- Mission Demolition Prototype/Assets/Slingshot.cs
+++ Mission Demolition Prototype/Assets/Slingshot.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+   
+   [Header("Set in Inspector")]
+   public GameObject prefabProjectile;
+
+   [Header("Set Dynamically")]
    public GameObject launchPoint;
+   public Vector3 launchPosition;
+   public GameObject projectile;
+   public bool aimingMode;
+
    private void Awake() {
        Transform launchPointTransform = transform.Find("LaunchPoint");
        launchPoint =  launchPointTransform.gameObject;
        launchPoint.SetActive(false);
+       launchPosition = launchPointTransform.position;
    }
+
    private void OnMouseEnter() {
        launchPoint.SetActive(true);
    }
+
    private void OnMouseExit() {
        launchPoint.SetActive(false);    
    }
+   private void OnMouseDown() {
+       aimingMode = true;
+       projectile = Instantiate(prefabProjectile) as GameObject;
+       projectile.transform.position = launchPosition;
+       projectile.GetComponent<Rigidbody>().isKinematic = true;
+   }
+
 }
